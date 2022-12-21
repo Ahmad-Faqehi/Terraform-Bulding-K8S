9fa1892c0f4f9d8288dc26d7fcf3a8d00cd24af8
Ahmad Faqehi <ahmadpsn7@gmail.com>
2022-12-21 20:16:48 +0300

    fix output pug

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "instance_msr_public_ip" {
   description = "Public IP address of master"
-  value       = aws_instance.ec2_instance_msr[0].public_ip
+  value       = aws_instance.ec2_instance_msr.public_ip
 }
 
 output "instance_msr_privte_ip" {
   description = "Private IP address of master"
-  value       = aws_instance.ec2_instance_msr[0].private_ip
+  value       = aws_instance.ec2_instance_msr.private_ip
 }
 output "instance_wrk_public_ip" {
   description = "Private IP address of master"
