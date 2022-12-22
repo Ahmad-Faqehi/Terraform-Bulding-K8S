f58386a34ddf9d92e694b27eb94998c9c3211f82
Ahmad Faqehi <ahmadpsn7@gmail.com>
2022-12-22 19:40:33 +0300

    Create variable of number of worker

--- variables.tf
+++ variables.tf
@@ -11,6 +11,12 @@ variable "secret_key" {  #Todo: uncomment the default value and add your secert 
 variable "ami_key_pair_name" { #Todo: uncomment the default value and add your pem key pair name. Hint: don't write '.pem' exction just the key name
         #default = "mykey" 
 }
+
+variable "number_of_worker" {
+        description = "number of worker instances to be join on cluster."
+        default = 2
+}
+
 variable "region" {
         description = "The region zone on AWS"
         default = "us-east-1" #The zone I selected is us-east-1, if you change it make sure to check if ami_id below is correct.
