9473061ea69a0d43ca251655675eabe4aaf515bc
Ahmad-Faqehi <ahmadpsn7@gmail.com>
2023-05-19 14:53:42 +0300

    edit the description of outputs

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "instance_msr_public_ip" {
-  description = "Public IP address of master"
+  description = "Public address IP of master"
   value       = aws_instance.ec2_instance_msr.public_ip
 }
 
 output "instance_wrks_public_ip" {
-  description = "Public IP address of master"
+  description = "Public address IP of worker"
   value       = aws_instance.ec2_instance_wrk.*.public_ip
 }
 
